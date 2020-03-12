9b09699facfd26a31bf94db592d78528497b3eaa
matzi24 <ggeomat@gmail.com>
2020-03-12 19:05:46 +0200

    Completed a14f2

---
+++ 2nd_Semester/Data_Structures/Exercise_2/a14f2.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+
+#define StackLimit 50   // το όριο μεγέθους της στοίβας, ενδεικτικά ίσο με 50
+
+
+typedef int StackElementType;   // ο τύπος των στοιχείων  της στοίβας
+                                //ενδεικτικά τύπος int
+typedef struct  {
+    int Top;
+    StackElementType Element[StackLimit];
+} StackType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void CreateStack(StackType *Stack);
+boolean EmptyStack(StackType Stack);
+boolean FullStack(StackType Stack);
+void Push(StackType *Stack, StackElementType Item);
+void Pop(StackType *Stack, StackElementType *Item);
+
+void FillStack(StackType *Stack);
+void ExecuteStack(StackType *Stack, StackElementType maxMemoryAdress);
+
+int main() {
+
+  printf("Please enter the maximum memory address: ");
+  StackElementType maxMemoryAdress;
+  scanf("%d",&maxMemoryAdress);
+
+  StackType callStack;
+  CreateStack(&callStack);
+  FillStack (&callStack);
+
+  ExecuteStack(&callStack, maxMemoryAdress);
+
+  return 0;
+}
+
+void CreateStack(StackType *Stack)
+/* Λειτουργία: Δημιουργεί μια κενή στοίβα.
+   Επιστρέφει: Κενή Στοίβα.*
+*/
+{
+    Stack -> Top = -1;
+   // (*Stack).Top = -1;
+}
+
+boolean EmptyStack(StackType Stack)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Ελέγχει αν η στοίβα Stack είναι κενή.
+   Επιστρέφει: True αν η Stack είναι κενή, False διαφορετικά
+*/
+{
+    return (Stack.Top == -1);
+}
+
+boolean FullStack(StackType Stack)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Ελέγχει αν η στοίβα Stack είναι γεμάτη.
+   Επιστρέφει: True αν η Stack είναι γεμάτη, False διαφορετικά
+*/
+{
+    return (Stack.Top == (StackLimit - 1));
+}
+
+void Push(StackType *Stack, StackElementType Item)
+/* Δέχεται: Μια στοίβα Stack και ένα στοιχείο Item.
+   Λειτουργία: Εισάγει το στοιχείο Item στην στοίβα Stack αν η Stack δεν είναι γεμάτη.
+   Επιστρέφει: Την τροποποιημένη Stack.
+   Έξοδος: Μήνυμα γεμάτης στοίβας, αν η στοίβα Stack είναι γεμάτη
+*/
+{
+    if (!FullStack(*Stack)) {
+        Stack -> Top++;
+        Stack -> Element[Stack -> Top] = Item;
+    } else
+        printf("Full Stack...");
+}
+
+void Pop(StackType *Stack, StackElementType *Item)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Διαγράφει το στοιχείο Item από την κορυφή της Στοίβας αν η Στοίβα δεν είναι κενή.
+   Επιστρέφει: Το στοιχείο Item και την τροποποιημένη Stack.
+   Έξοδος:  Μήνυμα κενής στοίβας αν η Stack είναι κενή
+*/
+{
+    if (!EmptyStack(*Stack)) {
+        *Item = Stack -> Element[Stack -> Top];
+        Stack -> Top--;
+    } else
+        printf("Empty Stack...");
+}
+
+void FillStack(StackType *Stack) {
+  StackElementType memoryAdress;
+  while (TRUE) {
+    printf("Please enter the next relative memory address: ");
+    scanf("%d",&memoryAdress);
+    if(memoryAdress==0) break;
+    Push(Stack,memoryAdress);
+  }
+}
+
+void ExecuteStack(StackType *Stack, StackElementType maxMemoryAdress){
+
+  StackElementType currentMemoryAddress;
+  printf("Please enter the current memory address: ");
+  scanf("%d",&currentMemoryAddress);
+
+  while (!EmptyStack(*Stack)) {
+
+    StackElementType currentInstruction;
+    Pop(Stack, &currentInstruction);
+    currentMemoryAddress+= currentInstruction;
+
+    if (currentMemoryAddress>=0 && currentMemoryAddress<=maxMemoryAdress) {
+        printf("Executing instruction: %d\n",currentMemoryAddress);
+    } else {
+      printf("Access Violation Exception at address: %d", currentMemoryAddress);
+      break;
+    }
+
+  }
+}
