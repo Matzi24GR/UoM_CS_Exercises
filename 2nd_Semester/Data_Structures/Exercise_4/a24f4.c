7552926e092f2f284dae1d3108528a6a8391f73f
matzi24 <ggeomat@gmail.com>
2020-04-02 20:39:21 +0300

    Added a24f4

---
+++ 2nd_Semester/Data_Structures/Exercise_4/a24f4.c
@@ -0,0 +1,283 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NumberOfNodes 20     //όριο μεγέθους της συνδεδεμένης λίστας,
+#define NilValue -1          // ειδική μεδενικη τιμη δείχνει το τέλος της Συνδ.λίστας
+
+typedef int ListElementType; /*τύπος δεδομένων για τα στοιχεία της συνδεδεμένης λίστας,
+                                ενδεικτικά επιλέχθηκε ο τύπος int */
+typedef int ListPointer;
+
+typedef struct {
+    ListElementType Data;
+    ListPointer  Next;
+} NodeType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr);
+void CreateLList(ListPointer *List);
+boolean EmptyLList(ListPointer List);
+boolean FullLList(ListPointer FreePtr);
+void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[]);
+void ReleaseNode(NodeType Node[NumberOfNodes], ListPointer P, ListPointer *FreePtr);
+void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item);
+void Delete(ListPointer *List, NodeType Node[], ListPointer *FreePtr, ListPointer PredPtr);
+void TraverseLinked(ListPointer List, NodeType Node[]);
+
+void menu(int *choice);
+void sort_list(ListPointer *List, NodeType Node[],boolean Ascending);
+void swap(ListPointer PrevPtr, ListPointer CurrentPtr,ListPointer NextPtr, NodeType Node[], ListPointer *List);
+
+int main() {
+
+  ListPointer AList;
+  NodeType Node[NumberOfNodes];
+  ListPointer FreePtr;
+
+  InitializeStoragePool(Node, &FreePtr);
+  CreateLList(&AList);
+
+  printf("Enter number of integers: ");
+  int listSize;
+  do {
+    scanf("%d", &listSize);
+  } while (listSize<1 || listSize>NumberOfNodes);
+
+  for (int i=0;i<listSize;i++){
+    printf("Enter an integer: ");
+    ListElementType element;
+    scanf("%d", &element);
+    Insert(&AList,Node,&FreePtr,-1,element);
+  }
+
+  int choice;
+  menu(&choice);
+
+  boolean Ascending = TRUE;
+  if (choice==2) Ascending = FALSE;
+
+  sort_list(&AList,Node,Ascending);
+
+  TraverseLinked(AList,Node);
+
+  return 0;
+}
+
+void menu(int *choice) {
+  printf("--------------------- Select ----------------------------\n");
+  printf("1. Sort Ascending  \n");
+  printf("2. Sort Descending\n");
+  int input;
+  do {
+    scanf("%d", &input);
+  } while (input!=1 && input!=2);
+  *choice = input;
+}
+
+void sort_list(ListPointer *List, NodeType Node[],boolean Ascending){
+  ListPointer NextPtr, CurrPtr, PrevPtr, OutPtr;
+  boolean toSwitch = FALSE;
+
+  if(!EmptyLList(*List)) {
+    OutPtr = *List;
+    while (OutPtr!=NilValue) {
+      CurrPtr = *List;
+      PrevPtr = NilValue;
+      NextPtr = Node[CurrPtr].Next;
+      while (NextPtr != NilValue) {
+        toSwitch = FALSE;
+        if (Ascending==TRUE) {
+          if (Node[CurrPtr].Data > Node[NextPtr].Data) {
+            toSwitch = TRUE;
+          }
+        } else {
+          if (Node[CurrPtr].Data < Node[NextPtr].Data) {
+            toSwitch = TRUE;
+          }
+        }
+
+        if (toSwitch == TRUE) {
+          swap(PrevPtr,CurrPtr,NextPtr,Node,List);
+          if (OutPtr==CurrPtr) {
+            OutPtr=NextPtr;
+          }
+        }
+
+        PrevPtr = CurrPtr;
+        CurrPtr = NextPtr;
+        NextPtr = Node[CurrPtr].Next;
+
+      }
+      OutPtr = Node[OutPtr].Next;
+    }
+
+  } else {
+    printf("Empty List ...\n");
+  }
+}
+
+void swap(ListPointer PrevPtr, ListPointer CurrentPtr,ListPointer NextPtr, NodeType Node[], ListPointer *List){
+  if (CurrentPtr==*List) {
+    *List = NextPtr;
+  }
+  Node[CurrentPtr].Next = Node[NextPtr].Next;
+  Node[NextPtr].Next = CurrentPtr;
+  if(PrevPtr!=NilValue) {
+    Node[PrevPtr].Next = NextPtr;
+  }
+}
+
+void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr)
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr που δείχνει στον
+                πρώτο διαθέσιμο κόμβο.
+  Λειτουργία: Αρχικοποιεί τον πίνακα Node ως συνδεδεμένη λίστα συνδέοντας μεταξύ
+                τους διαδοχικές εγγραφές του πίνακα,
+                και αρχικοποιεί τον δείκτη FreePtr .
+  Επιστρέφει: Τον τροποποιημένο πίνακα Node και τον
+                δείκτη FreePtr του πρώτου διαθέσιμου κόμβου
+*/
+{
+   int i;
+
+    for (i=0; i<NumberOfNodes-1;i++)
+    {
+        Node[i].Next=i+1;
+        Node[i].Data=NilValue;
+    }
+    Node[NumberOfNodes-1].Next=NilValue;
+    Node[NumberOfNodes-1].Data=NilValue;
+    *FreePtr=0;
+}
+
+void CreateLList(ListPointer *List)
+/* Λειτουργία: Δημιουργεί μια κενή συνδεδεμένη λίστα.
+   Επιστρέφει: Έναν (μηδενικό) δείκτη που δείχνει σε κενή λίστα
+*/
+{
+  *List=NilValue;
+}
+
+boolean EmptyLList(ListPointer List)
+/* Δέχεται:    Έναν δείκτη List που δείχνει σε μια συνδεδεμένη λίστα.
+   Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι κενή.
+   Επιστρέφει: True αν η συνδεδεμένη λίστα είναι κενή και false διαφορετικά
+*/
+{
+  return (List==NilValue);
+}
+
+boolean FullLList(ListPointer FreePtr)
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι γεμάτη.
+   Επιστρέφει: True αν η συνδεδεμένηλίστα είναι γεμάτη, false διαφορετικά
+*/
+{
+  return (FreePtr == NilValue);
+}
+
+void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[])
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr.
+  Λειτουργία: Αποκτά έναν "ελεύθερο" κόμβο που τον δείχνει ο δείκτης P.
+  Επιστρέφει: Τον δείκτη P και τον τροποποιημένο  δείκτη FreePtr
+                που δεικτοδοτεί στο πρώτο διαθέσιμο κόμβο
+*/
+{
+  *P = *FreePtr;
+  if (!FullLList(*FreePtr))
+    *FreePtr =Node[*FreePtr].Next;
+}
+
+void ReleaseNode(NodeType Node[], ListPointer P, ListPointer *FreePtr)
+/* Δέχεται:    Τον πίνακα Node, που αναπαριστά τη δεξαμενή των διαθέσιμων κόμβων,
+                έναν δείκτη TempPtr και τον δείκτη FreePtr.
+   Λειτουργία: Επιστρέφει στη δεξαμενή τον κόμβο στον οποίο δείχνει ο TempPtr.
+   Επιστρέφει: Τον τροποποιημένο πίνακα Node και τον δείκτη FreePtr
+*/
+{
+  Node[P].Next =*FreePtr;
+  //Node[P].Data = -1;
+  /* Οχι αναγκαία εντολή, βοηθητική για να φαίνονται στην εκτύπωση οι διαγραμμένοι κόμβοι */
+  *FreePtr =P;
+}
+
+void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item)
+/* Δέχεται:    Μια συνδεδεμένη λίστα, τον πίνακα Node, τον δείκτη PredPtr και
+                ένα στοιχείο Item.
+  Λειτουργία: Εισάγει στη συνδεδεμένη λίστα, αν δεν είναι γεμάτη, το στοιχείο
+                Item μετά από τον κόμβο στον οποίο δείχνει ο δείκτης PredPtr.
+  Επιστρέφει: Την τροποποιημένη συνδεδεμένη λίστα, τον τροποποιημένο πίνακα Node
+                και τον δείκτη FreePtr.
+  Εξοδος:     Μήνυμα γεμάτης λίστας, αν η συνδεδεμένη λίστα είναι γεμάτη
+*/
+{
+  ListPointer TempPtr;
+  GetNode(&TempPtr,FreePtr,Node);
+  if (!FullLList(TempPtr)) {
+    if (PredPtr==NilValue)
+    {
+        Node[TempPtr].Data =Item;
+        Node[TempPtr].Next =*List;
+        *List =TempPtr;
+    }
+    else
+      {
+        Node[TempPtr].Data =Item;
+        Node[TempPtr].Next =Node[PredPtr].Next;
+        Node[PredPtr].Next =TempPtr;
+      }
+}
+  else
+    printf("Full List ...\n");
+}
+
+void Delete(ListPointer *List, NodeType Node[], ListPointer *FreePtr, ListPointer PredPtr)
+/* Δέχεται:   Μια συνδεδεμένη λίστα και τον δείκτη PredPtr που δείχνει
+                στον προηγούμενο κόμβο από αυτόν που θα διαγραφεί.
+  Λειτουργία: Διαγράφει από τη συνδεδεμένη λίστα, αν δεν είναι κενή,
+                τον προηγούμενο κόμβο από αυτόν στον οποίο δείχνει ο PredPtr.
+  Επιστρέφει: Την τροποποιημένη λίστα και το δείκτη FreePtr.
+  Έξοδος:     Μήνυμα κενής λίστας, αν η συνδεδεμένη λίστα είναι κενή
+*/
+{
+  ListPointer TempPtr ;
+
+  if (!EmptyLList(*List))
+    if (PredPtr == NilValue)
+    {
+        TempPtr =*List;
+        *List =Node[TempPtr].Next;
+        ReleaseNode(Node,TempPtr,FreePtr);
+    }
+    else
+      {
+        TempPtr =Node[PredPtr].Next;
+        Node[PredPtr].Next =Node[TempPtr].Next;
+        ReleaseNode(Node,TempPtr,FreePtr);
+      }
+  else
+    printf("Empty List ...\n");
+}
+
+void TraverseLinked(ListPointer List, NodeType Node[])
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Κάνει διάσχιση της συνδεδεμένης λίστας, αν δεν είναι κενή.
+   Έξοδος:     Εξαρτάται από την επεξεργασία
+*/
+{
+  ListPointer CurrPtr;
+
+  if (!EmptyLList(List))
+  {
+      CurrPtr =List;
+      while (CurrPtr != NilValue)
+      {
+          printf("%d ",Node[CurrPtr].Data);
+          CurrPtr=Node[CurrPtr].Next;
+       }
+       printf("\n");
+   }
+  else printf("Empty List ...\n");
+}
