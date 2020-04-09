9ef97373f4f2391fa1c7d682e2901b24cd2e3016
matzi24 <ggeomat@gmail.com>
2020-04-09 18:40:30 +0300

    Added a2af4

---
+++ 2nd_Semester/Data_Structures/Exercise_5/a2af4.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ListElementType;           /* ο τύπος των στοιχείων της συνδεδεμένης λίστας
+                                        ενδεικτικά τύπου int */
+typedef struct ListNode *ListPointer;   //ο τύπος των δεικτών για τους κόμβους
+typedef struct ListNode
+{
+	ListElementType Data;
+    ListPointer Next;
+} ListNode;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+
+void CreateList(ListPointer *List);
+boolean EmptyList(ListPointer List);
+void LinkedInsert(ListPointer *List, ListElementType Item, ListPointer PredPtr);
+void LinkedDelete(ListPointer *List, ListPointer PredPtr);
+void LinkedTraverse(ListPointer List);
+void LinearSearch(ListPointer List, ListElementType Item, ListPointer *PredPtr, boolean *Found);
+void OrderedLimearSearch(ListPointer List, ListElementType Item, ListPointer *PredPtr, boolean *Found);
+
+void append_list_element(ListPointer *List, ListElementType Item);
+
+int main() {
+
+  ListPointer AList, PredPtr;
+  ListElementType Item;
+
+  CreateList(&AList);
+
+  printf("DWSE TO PLH8OS TWN STOIXEIWN THS LISTAS: ");
+  int inputCount;
+  scanf("%d", &inputCount);
+
+  for (int i=0; i<inputCount;i++) {
+    printf("DWSE TON ARI8MO GIA EISAGWGH STHN ARXH THS LISTAS: ");
+    scanf("%d", &Item);
+
+    LinkedInsert(&AList, Item, NULL);
+  }
+
+  LinkedTraverse(AList);
+
+  printf("\nDWSE TON ARI8MO GIA EISAGWGH STO TELOS THS LISTAS: ");
+  scanf("%d", &Item);
+
+  append_list_element(&AList, Item);
+
+  LinkedTraverse(AList);
+
+  return 0;
+}
+
+void append_list_element(ListPointer *List, ListElementType Item){
+  ListPointer CurrPtr = *List;
+  ListPointer PrevPtr;
+  while ( CurrPtr!=NULL )
+  {
+       //printf("%d ",(*CurrPtr).Data);
+       PrevPtr = CurrPtr;
+       CurrPtr = CurrPtr->Next;
+  }
+  LinkedInsert(List, Item, PrevPtr);
+}
+
+void CreateList(ListPointer *List)
+/* Λειτουργία: Δημιουργεί μια κενή συνδεδεμένη λίστα.
+  Επιστρέφει:  Τον μηδενικό δείκτη List
+*/
+{
+	*List = NULL;
+}
+
+boolean EmptyList(ListPointer List)
+/* Δέχεται:   Μια συνδεδεμένη λίστα με τον List να δείχνει στον πρώτο κόμβο.
+  Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι κενή.
+  Επιστρέφει: True αν η λίστα είναι κενή και false διαφορετικά
+*/
+{
+	return (List==NULL);
+}
+
+void LinkedInsert(ListPointer *List, ListElementType Item, ListPointer PredPtr)
+/* Δέχεται:    Μια συνδεδεμένη λίστα με τον List να δείχνει στον πρώτο κόμβο,
+                ένα στοιχείο δεδομένων Item και έναν δείκτη PredPtr.
+   Λειτουργία: Εισάγει έναν κόμβο, που περιέχει το Item, στην συνδεδεμένη λίστα
+                μετά από τον κόμβο που δεικτοδοτείται από τον PredPtr
+                ή στην αρχή  της συνδεδεμένης λίστας,
+                αν ο PredPtr είναι μηδενικός(NULL).
+  Επιστρέφει:  Την τροποποιημένη συνδεδεμένη λίστα με τον πρώτο κόμβο της
+                να δεικτοδοτείται από τον List.
+*/
+{
+	ListPointer TempPtr;
+
+   TempPtr= (ListPointer)malloc(sizeof(struct ListNode));
+ /*  printf("Insert &List %p, List %p,  &(*List) %p, (*List) %p, TempPtr %p\n",
+   &List, List,  &(*List), (*List), TempPtr); */
+   TempPtr->Data = Item;
+	if (PredPtr==NULL) {
+        TempPtr->Next = *List;
+        *List = TempPtr;
+    }
+    else {
+        TempPtr->Next = PredPtr->Next;
+        PredPtr->Next = TempPtr;
+    }
+}
+
+void LinkedTraverse(ListPointer List)
+/* Δέχεται:    Μια συνδεδεμένη λίστα με τον List να δείχνει στον πρώτο κόμβο.
+   Λειτουργία: Διασχίζει τη συνδεδεμένη λίστα και
+                επεξεργάζεται κάθε δεδομένο ακριβώς μια φορά.
+   Επιστρέφει: Εξαρτάται από το είδος της επεξεργασίας.
+*/
+{
+	ListPointer CurrPtr;
+
+   if (EmptyList(List))
+   	    printf("EMPTY LIST\n");
+   else
+   {
+   	    CurrPtr = List;
+         while ( CurrPtr!=NULL )
+        {
+             printf("%d ",(*CurrPtr).Data);
+             CurrPtr = CurrPtr->Next;
+        }
+   }
+}
