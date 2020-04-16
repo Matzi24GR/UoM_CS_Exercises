0268806c0121318de31448a5805ccd295d9ad878
matzi24 <ggeomat@gmail.com>
2020-04-16 17:46:24 +0300

    Added a14f4

---
+++ 2nd_Semester/Data_Structures/Exercise_6/a14f4.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+
+#define StackLimit 50
+
+typedef char StackElementType;
+
+typedef struct  {
+    int Top;
+    StackElementType Element[StackLimit];
+} StackType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void CreateStack(StackType *Stack);
+boolean EmptyStack(StackType Stack);
+boolean FullStack(StackType Stack);
+void Push(StackType *Stack, StackElementType Item);
+void Pop(StackType *Stack, StackElementType *Item);
+
+int main() {
+
+  StackType AStack;
+  char str[40];
+  long strLen;
+  boolean isCorrect = TRUE;
+
+  CreateStack(&AStack);
+
+  printf("EISAGETE MIA PARASTASI: ");
+  scanf("%s", str);
+  strLen = strlen(str);
+
+  for (int i = 0; i < strLen; i++) {
+
+    if (str[i] == '(' || str[i] == '[' || str[i] == '{') {
+
+      Push(&AStack, str[i]);
+
+    } else if (str[i] == ')' || str[i] == ']' || str[i] == '}') {
+
+      if (EmptyStack(AStack)) {
+
+        isCorrect = FALSE;
+        break;
+
+      } else {
+
+        StackElementType poppedItem;
+        Pop(&AStack, &poppedItem);
+
+        if (str[i]==')'&&poppedItem!='(' ||
+            str[i]==']'&&poppedItem!='[' ||
+            str[i]=='}'&&poppedItem!='{'   )
+        {
+          isCorrect = FALSE;
+          break;
+        }
+      }
+
+    }
+
+  }
+
+  if (isCorrect && EmptyStack(AStack)) {
+    printf("CORRECT");
+  } else {
+    printf("WRONG");
+  }
+
+  return 0;
+}
+
+void CreateStack(StackType *Stack)
+/* Λειτουργία: Δημιουργεί μια κενή στοίβα.
+   Επιστρέφει: Κενή Στοίβα.*
+*/
+{
+    Stack -> Top = -1;
+   // (*Stack).Top = -1;
+}
+
+boolean EmptyStack(StackType Stack)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Ελέγχει αν η στοίβα Stack είναι κενή.
+   Επιστρέφει: True αν η Stack είναι κενή, False διαφορετικά
+*/
+{
+    return (Stack.Top == -1);
+}
+
+boolean FullStack(StackType Stack)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Ελέγχει αν η στοίβα Stack είναι γεμάτη.
+   Επιστρέφει: True αν η Stack είναι γεμάτη, False διαφορετικά
+*/
+{
+    return (Stack.Top == (StackLimit - 1));
+}
+
+void Push(StackType *Stack, StackElementType Item)
+/* Δέχεται: Μια στοίβα Stack και ένα στοιχείο Item.
+   Λειτουργία: Εισάγει το στοιχείο Item στην στοίβα Stack αν η Stack δεν είναι γεμάτη.
+   Επιστρέφει: Την τροποποιημένη Stack.
+   Έξοδος: Μήνυμα γεμάτης στοίβας, αν η στοίβα Stack είναι γεμάτη
+*/
+{
+    if (!FullStack(*Stack)) {
+        Stack -> Top++;
+        Stack -> Element[Stack -> Top] = Item;
+    } else
+        printf("Full Stack...");
+}
+
+void Pop(StackType *Stack, StackElementType *Item)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Διαγράφει το στοιχείο Item από την κορυφή της Στοίβας αν η Στοίβα δεν είναι κενή.
+   Επιστρέφει: Το στοιχείο Item και την τροποποιημένη Stack.
+   Έξοδος:  Μήνυμα κενής στοίβας αν η Stack είναι κενή
+*/
+{
+    if (!EmptyStack(*Stack)) {
+        *Item = Stack -> Element[Stack -> Top];
+        Stack -> Top--;
+    } else
+        printf("Empty Stack...");
+}
