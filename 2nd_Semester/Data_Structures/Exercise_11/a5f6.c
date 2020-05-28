a9a90dd8753fb564a5414af90a32a297355ae021
matzi24 <ggeomat@gmail.com>
2020-05-28 03:12:58 +0300

    Added a5f6

---
+++ 2nd_Semester/Data_Structures/Exercise_11/a5f6.c
@@ -0,0 +1,266 @@
+#include <stdio.h>
+#include <string.h>
+
+#define HMax 10              /*     το μέγεθος του πίνακα HashTable
+                                ενδεικτικά ίσο με 5 */
+#define VMax 100            /*το μέγεθος της λίστας,
+                                ενδεικτικά ίσο με 30 */
+#define EndOfList -1        /* σημαία που σηματοδοτεί το τέλος της λίστας
+                                και της κάθε υπολίστας συνωνύμων */
+
+typedef int ListElementType;  /*τύπος δεδομένων για τα στοιχεία της λίστας
+                               * ενδεικτικά τύπου int */
+
+typedef struct {
+  char RecKey[8];
+  char password[6];
+  int Link;
+} ListElm;
+
+typedef struct  {
+	int HashTable[HMax];   // πίνακας δεικτών προς τις υπολίστες συνωνύμων
+    int Size;                // πλήθος εγγραφών της λίστας List
+	int SubListPtr;          // Dδείκτης σε μια υπολίστα συνωνύμων
+    int StackPtr;           // δείκτης προς την πρώτη ελεύθερη θέση της λίστας List
+	ListElm List[VMax];
+} HashListType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void CreateHashList(HashListType *HList);
+int HashKey(char Key[]);
+boolean FullHashList(HashListType HList);
+void SearchSynonymList(HashListType HList,char KeyArg[],int *Loc,int *Pred);
+void SearchHashList(HashListType HList,char KeyArg[],int *Loc,int *Pred);
+void AddRec(HashListType *HList,ListElm InRec);
+
+int findAverage(char Key[]);
+void BuildHashList(HashListType *HList);
+
+
+int main() {
+
+  HashListType HashList;
+  BuildHashList(&HashList);
+
+  char choice;
+  do {
+    ListElm userElem;
+
+    printf("USERNAME: ");
+    scanf(" %s", &userElem.RecKey);
+
+    printf("PASSWORD: ");
+    scanf(" %s", &userElem.password);
+
+    int Loc, Pred;
+    SearchHashList(HashList, userElem.RecKey, &Loc, &Pred);
+
+    if(Loc != -1) {
+
+      if(strcmp(HashList.List[Loc].password, userElem.password) == 0) {
+          printf("You have logged in to the system\n");
+      } else {
+          printf("Access is forbidden: Wrong password\n");
+      }
+
+    } else{
+      printf("Access is forbidden: Wrong user ID\n");
+    }
+
+    printf("New entry Y/N (Y=Yes, N=No)? ");
+    scanf(" %c", &choice);
+
+  } while(choice != 'N');
+
+
+  return 0;
+}
+
+int findAverage(char Key[]){
+  int first = Key[0];
+  int last = Key[strlen(Key)-1];
+  return (first+last) / 2;
+}
+
+void BuildHashList(HashListType *HList){
+
+  CreateHashList(HList);
+
+  FILE *input = fopen("I5f6.txt", "r");
+
+  while(TRUE) {
+
+    ListElm AnItem;
+
+    int nscan = fscanf(input, "%s %s", AnItem.RecKey, AnItem.password);
+
+    if (nscan == EOF) break;
+
+    if (nscan != 2){
+        printf("Improper file format\n");
+      break;
+    } else{
+        AddRec(HList, AnItem);
+    }
+
+  }
+
+}
+
+//------------------------------------------------------------------------------------------
+
+int HashKey(char Key[])
+/* Δέχεται:    Την τιμή Key ενός κλειδιού.
+   Λειτουργία: Βρίσκει την τιμή κατακερματισμού HValue για το κλειδί Key.
+   Επιστρέφει: Την τιμή κατακερματισμού HValue
+*/
+{
+  int avg = findAverage(Key);
+	return avg % HMax;
+}
+
+void CreateHashList(HashListType *HList)
+/* Λειτουργία: Δημιουργεί μια δομή HList.
+   Επιστρέφει: Την δομή HList
+*/
+{
+	int index;
+
+	HList->Size=0;           //ΔΗΜΙΟΥΡΓΕΙ ΜΙΑ ΚΕΝΗ ΛΙΣΤΑ
+	HList->StackPtr=0;       //ΔΕΙΚΤΗς ΣΤΗ ΚΟΡΥΦΗ ΤΗΣ ΣΤΟΙΒΑΣ ΤΩΝ ΕΛΕΥΘΕΡΩΝ ΘΕΣΕΩΝ
+
+    /*ΑΡΧΙΚΟΠΟΙΕΙ ΤΟΝ ΠΙΝΑΚΑ HashTable ΤΗΣ ΔΟΜΗΣ HList ΩΣΤΕ ΚΑΘΕ ΣΤΟΙΧΕΙΟΥ ΤΟΥ
+        ΝΑ ΕΧΕΙ ΤΗ ΤΙΜΗ EndOfList (-1)*/
+    index=0;
+	while (index<HMax)
+	{
+		HList->HashTable[index]=EndOfList;
+		index=index+1;
+    }
+
+     //Δημιουργία της στοίβας των ελεύθερων θέσεων στη λίστα HList
+    index=0;
+	while(index < VMax-1)
+	{
+		HList->List[index].Link=index+1;
+		//HList->List[index].Data=0;
+		index=index+1;
+	}
+	HList->List[index].Link=EndOfList;
+/*
+	printf("HashTable\n");
+	for (index=0;index<HMax;index++)
+	   printf("%d: %d\n",index,HList->HashTable[index]);
+
+    printf("List of keys\n");
+	for (index=0;index<=VMax-1;index++)
+	   printf("%d\t%d\n",index,HList->List[index].Link);
+	   */
+}
+
+boolean FullHashList(HashListType HList)
+/* Δέχεται:    Μια δομή HList.
+   Λειτουργία: Ελέγχει αν η λίστα List της δομής HList είναι γεμάτη.
+   Επιστρέφει: TRUE αν η λίστα List είναι γεμάτη, FALSE διαφορετικά.
+*/
+{
+	return(HList.Size==VMax);
+}
+
+void SearchSynonymList(HashListType HList,char KeyArg[],int *Loc,int *Pred)
+/* Δέχεται:     Μια δομή HList και μια τιμή κλειδιού KeyArg.
+    Λειτουργία: Αναζητά μια εγγραφή με κλειδί KeyArg στην υπολίστα συνωνύμων.
+    Επιστρέφει: Τη θέση Loc της εγγραφής και τη θέση Pred της προηγούμενης
+                εγγραφής στην υπολίστα
+*/
+{
+	int Next;
+	Next=HList.SubListPtr;
+	*Loc=-1;
+	*Pred=-1;
+	while(Next!=EndOfList)
+	{
+		if (strcmp(HList.List[Next].RecKey, KeyArg) == 0)
+		{
+			*Loc=Next;
+			Next=EndOfList;
+		}
+		else
+		{
+			*Pred=Next;
+			Next=HList.List[Next].Link;
+		}
+	}
+}
+void SearchHashList(HashListType HList,char KeyArg[],int *Loc,int *Pred)
+/* Δέχεται:     Μια δομή HList και μια τιμή κλειδιού KeyArg.
+    Λειτουργία: Αναζητά μια εγγραφή με κλειδί KeyArg στη δομή HList.
+    Επιστρέφει: Τη θέση Loc της εγγραφής και τη θέση Pred της
+                προηγούμενης εγγραφής της υπολίστας στην οποία ανήκει.
+                Αν δεν υπάρχει εγγραφή με κλειδί KeyArg τότε Loc=Pred=-1
+*/
+{
+	int HVal;
+	HVal=HashKey(KeyArg);
+    if (HList.HashTable[HVal]==EndOfList)
+	{
+		*Pred=-1;
+		*Loc=-1;
+	}
+	else
+	{
+		HList.SubListPtr=HList.HashTable[HVal];
+		SearchSynonymList(HList,KeyArg,Loc,Pred);
+	}
+}
+
+void AddRec(HashListType *HList,ListElm InRec)
+/* Δέχεται:    Μια δομή HList και μια εγγραφή InRec.
+   Λειτουργία: Εισάγει την εγγραφή InRec στη λίστα List, αν δεν είναι γεμάτη,
+                και ενημερώνει τη δομή HList.
+   Επιστρέφει: Την τροποποιημένη δομή HList.
+   Έξοδος:     Μήνυμα γεμάτης λίστας, αν η List είναι γεμάτη, διαφορετικά,
+                αν υπάρχει ήδη εγγραφή με το ίδιο κλειδί,
+                εμφάνιση αντίστοιχου μηνύματος
+*/
+{
+	int Loc, Pred, New, HVal;
+
+   // New=0;
+	if(!(FullHashList(*HList)))
+	{
+		Loc=-1;
+		Pred=-1;
+		SearchHashList(*HList,InRec.RecKey,&Loc,&Pred);
+		if(Loc==-1)
+		{
+			HList->Size=HList->Size +1;
+			New=HList->StackPtr;
+			HList->StackPtr=HList->List[New].Link;
+			HList->List[New]=InRec;
+			if (Pred==-1)
+			{
+			    HVal=HashKey(InRec.RecKey);
+                HList->HashTable[HVal]=New;
+				HList->List[New].Link=EndOfList;
+			}
+			else
+			{
+                HList->List[New].Link=HList->List[Pred].Link;
+				HList->List[Pred].Link=New;
+			}
+		}
+
+		else
+		{
+			printf("YPARXEI HDH EGGRAFH ME TO IDIO KLEIDI \n");
+		}
+	}
+	else
+	{
+		printf("Full list...");
+	}
+}
