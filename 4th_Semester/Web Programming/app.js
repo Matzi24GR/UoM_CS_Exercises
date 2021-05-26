a837772fc52b2e3aa6fe37a86285fba1ca7a0382
Matzi24GR <ggeomat@gmail.com>
2021-05-26 22:38:45 +0300

    Added Web Programming HTML exercise

--- 4th_Semester/Web Programming/app.js
+++ 4th_Semester/Web Programming/app.js
@@ -1,11 +1,52 @@
 'use strict'
 const img = document.querySelector('img');
-if (document.querySelector('input[name="color"]')) {
-    document.querySelectorAll('input[name="color"]').forEach((elem) => {
-      elem.addEventListener("change", function(event) {
-        var item = event.target.value;
-        console.log(item);
-        img.setAttribute('src', 'img/case_'+item+'.jpeg');
-      });
-    });
-  }
+const priceText = document.getElementById('priceText');
+const baseCost = 200;
+
+let processorCost = 0;
+let graphicsCost = 0;
+let ramCost = 0;
+let storageCost = 0;
+
+refreshCost();
+
+document.querySelectorAll('input[name="color"]').forEach((elem) => {
+  elem.addEventListener("change", function(event) {
+    var item = event.target.value;
+    console.log(item);
+    img.setAttribute('src', 'img/case_'+item+'.jpeg');
+  });
+})
+
+document.querySelectorAll('input[name="processor"]').forEach((elem) => {
+  elem.addEventListener("change", function(event) {
+    processorCost = event.target.value;
+    refreshCost();
+  });
+})
+
+document.querySelectorAll('input[name="graphics"]').forEach((elem) => {
+  elem.addEventListener("change", function(event) {
+    graphicsCost = event.target.value;
+    refreshCost();
+  });
+})
+
+document.querySelectorAll('input[name="ram"]').forEach((elem) => {
+  elem.addEventListener("change", function(event) {
+    ramCost = event.target.value;
+    refreshCost();
+  });
+})
+
+document.querySelectorAll('input[name="storage"]').forEach((elem) => {
+  elem.addEventListener("change", function(event) {
+    storageCost = event.target.value;
+    refreshCost();
+  });
+})
+
+function refreshCost() {
+  let total = baseCost + +processorCost + +graphicsCost + +ramCost + +storageCost;
+  priceText.innerText = "Price: $" + total;
+}
